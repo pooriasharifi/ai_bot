fd341a4e022c910fbe24b60fc8f228404495c203
‘pooria’ <pooriasharifi577@gmail.com>
2022-03-04 01:15:53 +0330

    final

--- lib/screen/controllers/profileController.dart
+++ lib/screen/controllers/profileController.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 
 class ProfileController extends GetxController{
   RxBool loading = true.obs;
+  RxBool password = true.obs;
+
 
   profileHandle() async {
     await Future.delayed(Duration(seconds: 2));
--- lib/screen/views/profile.dart
+++ lib/screen/views/profile.dart
@@ -1,5 +1,7 @@
 
+import 'package:ai_bot/modules/button.dart';
 import 'package:ai_bot/modules/constans.dart';
+import 'package:ai_bot/modules/textFeild.dart';
 import 'package:ai_bot/screen/controllers/profileController.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -16,7 +18,7 @@ class ProfilePage extends GetView{
  Widget _builBody() {
    ProfileController controller =Get.put(ProfileController());
 controller.profileHandle();
-   return Obx((){
+   return GetBuilder<ProfileController>(builder: (controller){
       if(controller.loading.value){
         return Center(
           child: SpinKitFadingCircle(
@@ -27,6 +29,66 @@ controller.profileHandle();
         appBar: AppBar(
           title: Text('Profile'),
 
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: SingleChildScrollView(
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 40),
+                    child: Center(
+                      child: CircleAvatar(
+                        maxRadius: 80,
+                        backgroundColor: backLight,
+                        child: Text('P',style: Get.textTheme.headline1,),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text('Email',style: Get.textTheme.headline3,),
+                  ),
+                  Btn(
+                    hasBorder: true,
+                    title: 'example@mail.com',
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text('Expiration',style: Get.textTheme.headline3,),
+                  ),
+                  Btn(
+                    hasBorder: true,
+                    title: '22/10/2022',
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Row(
+                      children: [
+                        Text('Password',style: Get.textTheme.headline3,),
+                        SizedBox(width: 5,),
+                        GestureDetector(
+                            onTap: (){
+                              controller.password.value?controller.password.value=false:controller.password.value=true;
+                              controller.refresh();
+                            },
+                            child: Icon(Icons.remove_red_eye_rounded,color: sec,))
+                      ],
+                    ),
+                  ),
+                  Btn(
+                    hasBorder: true,
+                    title: controller.password.value?'**********':'123456789',
+
+                  ),
+
+                ],
+              ),
+            ),
+          ),
         ),
       );
     });
